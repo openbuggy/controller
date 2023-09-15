72059415642d86c7dd35e194019a928dc6db3df7
Moritz Thüning <moritz.thuening@gmail.com>
2023-09-15 21:51:36 +0200

    measure battery voltage

--- speedcontrol.c
+++ speedcontrol.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 #include "pico/cyw43_arch.h"
 
 static const uint32_t CPU_FREQ = 125000000;
@@ -14,13 +16,19 @@ static const uint16_t PWM_SET_LEVEL_FREQ = 30;
 static const uint16_t MAX_TIME_WITHOUT_UPDATE_MS = 100;
 static const float NEUTRAL_THROTTLE = 0.5f;
 static const float NEUTRAL_STEERING = 0.5f;
+static const float BATTERY_A_FACTOR = 0.00117307136;
+static const float BATTERY_B_FACTOR = 0.00111691884;
+static const float BATTERY_MEASURE_FREQ = 1;
+static const float BATTERY_SEND_INTERVALL_S = 5;
 
 static const float PWM_MS_LEVEL = CPU_FREQ / PWM_CLKDIV / 1000.f;
 static const uint16_t MAX_COUNTER_WITHOUT_UPDATE = MAX_TIME_WITHOUT_UPDATE_MS / 1000.f * PWM_SET_LEVEL_FREQ;
+static const uint16_t BATTERY_SEND_COUNTER = BATTERY_SEND_INTERVALL_S * BATTERY_MEASURE_FREQ;
 
 static volatile float throttle = NEUTRAL_THROTTLE;
 static volatile float steering = NEUTRAL_STEERING;
 static volatile uint16_t set_level_counter = 0;
+static volatile uint16_t measure_battery_counter = 0;
 
 bool set_levels_callback(struct repeating_timer *t)
 {
@@ -36,6 +44,21 @@ bool set_levels_callback(struct repeating_timer *t)
         steering_level = 0;
     }
     pwm_set_both_levels(1, throttle_level, steering_level);
+    return true;
+}
+
+bool measure_battery_callback(struct repeating_timer *t)
+{
+    measure_battery_counter++;
+    adc_select_input(0);
+    const float voltage_a = adc_read() * BATTERY_A_FACTOR;
+    adc_select_input(1);
+    const float voltage_b = adc_read() * BATTERY_B_FACTOR;
+    if (measure_battery_counter == BATTERY_SEND_COUNTER) {
+        const uint16_t buffer[] = {(uint16_t)(voltage_a * 100), (uint16_t)(voltage_b * 100)};
+        fwrite(buffer, 2, sizeof(buffer[0]), stdout);
+        measure_battery_counter = 0;
+    }
     return true;
 }
 
@@ -59,6 +82,16 @@ int main()
 
     stdio_init_all();
     cyw43_arch_init();
+
+    adc_init();
+    // Make sure GPIO is high-impedance, no pullups etc
+    adc_gpio_init(26);
+    adc_gpio_init(27);
+    struct repeating_timer battery_timer;
+    // Negative delay so means we will call repeating_timer_callback, and call it again
+    // 500ms later regardless of how long the callback took to execute
+    add_repeating_timer_ms(-1000 / BATTERY_MEASURE_FREQ, measure_battery_callback, NULL, &battery_timer);
+
     bool led_on = false;
 
     while (true)
